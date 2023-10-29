e239b811d9bbb545b4f9feeea3d4cbed8491c930
JasonChen <aawe44@gmail.com>
2023-10-29 11:02:04 -0400

    update Order.java

--- trade-order/src/main/java/com/jason/trade/order/db/model/Order.java
+++ trade-order/src/main/java/com/jason/trade/order/db/model/Order.java
@@ -1,14 +1,12 @@
 package com.jason.trade.order.db.model;
 
 import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
-@Getter
 @Builder
+@Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Order {
     private Long id;
 
@@ -28,39 +26,4 @@ public class Order {
 
     private Date createTime;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setGoodsId(Long goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public void setPayPrice(Integer payPrice) {
-        this.payPrice = payPrice;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public void setActivityId(Long activityId) {
-        this.activityId = activityId;
-    }
-
-    public void setActivityType(Integer activityType) {
-        this.activityType = activityType;
-    }
-
-    public void setPayTime(Date payTime) {
-        this.payTime = payTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
