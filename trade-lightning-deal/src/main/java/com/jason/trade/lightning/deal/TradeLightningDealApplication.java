f5017c0797f6ff41670e28ebc782aa5340e41bc5
JasonChen <aawe44@gmail.com>
2023-10-30 21:42:19 -0400

    create trade-lightning-deal module

---
+++ trade-lightning-deal/src/main/java/com/jason/trade/lightning/deal/TradeLightningDealApplication.java
@@ -0,0 +1,13 @@
+package com.jason.trade.lightning.deal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeLightningDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeLightningDealApplication.class, args);
+	}
+
+}
