52241cfedc0ef9336c04b3aa796b7d9dc70ade83
JasonChen <aawe44@gmail.com>
2023-10-30 21:56:35 -0400

    setup database config

--- trade-lightning-deal/src/main/java/com/jason/trade/lightning/deal/TradeLightningDealApplication.java
+++ trade-lightning-deal/src/main/java/com/jason/trade/lightning/deal/TradeLightningDealApplication.java
@@ -1,8 +1,12 @@
 package com.jason.trade.lightning.deal;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.jason"})
+@MapperScan({"com.jason.trade.lightning.deal.db.mappers"})
 @SpringBootApplication
 public class TradeLightningDealApplication {
 
---
+++ trade-lightning-deal/src/main/java/com/jason/trade/lightning/deal/db/mappers/SeckillActivityMapper.java
@@ -0,0 +1,17 @@
+package com.jason.trade.lightning.deal.db.mappers;
+
+import com.jason.trade.lightning.deal.db.model.SeckillActivity;
+
+public interface SeckillActivityMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SeckillActivity record);
+
+    int insertSelective(SeckillActivity record);
+
+    SeckillActivity selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SeckillActivity record);
+
+    int updateByPrimaryKey(SeckillActivity record);
+}
