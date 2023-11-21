683399f938184bf1e2c1872b0a0a5939e41b1f05
JasonChen <aawe44@gmail.com>
2023-11-20 20:39:22 -0500

    create trade common module

---
+++ trade-common/src/test/java/com/jason/trade/common/TradeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.trade.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
