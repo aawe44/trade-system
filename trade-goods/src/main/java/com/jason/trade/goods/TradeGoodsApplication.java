8d663c78ac2647af8eb4c6aa693534929ca57806
JasonChen <aawe44@gmail.com>
2023-11-20 22:00:56 -0500

    refactor for TradeGoodsApplication

--- trade-goods/src/main/java/com/jason/trade/goods/TradeGoodsApplication.java
+++ trade-goods/src/main/java/com/jason/trade/goods/TradeGoodsApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.jason"})
 @MapperScan({"com.jason.trade.goods.db.mappers"})
 @SpringBootApplication
--- trade-goods/src/main/java/com/jason/trade/goods/controller/GoodsController.java
+++ trade-goods/src/main/java/com/jason/trade/goods/controller/GoodsController.java
@@ -1,17 +1,113 @@
 package com.jason.trade.goods.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.jason.trade.goods.db.model.Goods;
+import com.jason.trade.goods.service.GoodsService;
+import com.jason.trade.goods.service.SearchService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 
 @Slf4j
 @Controller
 public class GoodsController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @Autowired
+    private SearchService searchService;
 
     @RequestMapping("/goods/test")
     @ResponseBody
     public String test() {
         return "hello word java!";
     }
+    /**
+     * @param goods
+     * @return
+     */
+    @GetMapping("/goods/insertGoods")
+    @ResponseBody
+    public boolean insertGoods(@RequestBody Goods goods) {
+        log.info("insertGoods goods:{}", JSON.toJSON(goods));
+        return goodsService.insertGoods(goods);
+    }
+
+    /**
+     * 查询商品信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/goods/queryGoodsById")
+    @ResponseBody
+    public Goods queryGoodsById(long id) {
+        log.info("queryGoodsById id:{}", id);
+        return goodsService.queryGoodsById(id);
+    }
+
+
+    /**
+     * 根据关键词搜索
+     *
+     * @param keyword
+     * @param from
+     * @param size
+     * @return
+     */
+    @GetMapping("/goods/searchGoodsList")
+    @ResponseBody
+    public List<Goods> searchGoodsList(String keyword, int from, int size) {
+        log.info("searchGoodsList keyword:{} from:{} size:{}", keyword, from, size);
+        return searchService.searchGoodsList(keyword, from, size);
+    }
+
+
+    /**
+     * 锁定商品的库存
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/goods/lockStock")
+    @ResponseBody
+    public boolean lockStock(long id) {
+        log.info("lockStock id:{}", id);
+        return goodsService.lockStock(id);
+    }
+
+    /**
+     * 库存扣减
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/goods/deductStock")
+    @ResponseBody
+    public boolean deductStock(long id) {
+        log.info("deductStock id:{}", id);
+        return goodsService.deductStock(id);
+    }
+
+    /**
+     * 锁定的库存回补
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/goods/revertStock")
+    @ResponseBody
+    public boolean revertStock(long id) {
+        log.info("revertStock id:{}", id);
+        return goodsService.revertStock(id);
+    }
 }
+
